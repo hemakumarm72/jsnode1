c3d505aa02421c0e42534d3ce8ad76ed745c9ca8
hemakumarm72 <hemakumarm72@gmail.com>
2020-09-04 17:02:37 +0530

    mysql bundler insert data

---
+++ bundler_insertsql.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database:'testdb1',
+    password:''
+})
+
+connection.connect(function(err){
+    (err)
+    if (err) throw err
+    console.log('connect database successful')
+    var sql =  "insert into Person1 (personid, firstname, lastname) values ?";
+    var values = [
+        ['233', 'Chaal', 'CTO'],
+        ['23', 'Pritam', 'Android Developer'],
+        ['234234' , 'Venkatesh', 'Frontend Developer'],
+        ['234234', 'Niranjan', 'Backend Developer']
+    ];
+    connection.query(sql, [values],function(err, result){
+        if (err) throw err
+       
+        console.log('Insert successful'+ result.affectedRows + ","+ result.insertId);
+    })
+});
+
+
